bea432dd54aac8ac281facc8d66eac8bb36ab7dc
oscar anillo <oscarani14@gmail.com>
2022-08-17 12:19:46 -0500

    add imageComponent and its styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import ImageComponent from './components/img-component';
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-
+        <ImageComponent />
       </div>
     </div>
   );

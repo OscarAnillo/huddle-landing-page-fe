b050797acb77587726f6ba2921c80dd8ce35d992
oscar anillo <oscarani14@gmail.com>
2022-08-17 13:20:57 -0500

    add styles to textComponent

--- src/components/text-component.js
+++ src/components/text-component.js
@@ -2,14 +2,14 @@ import { BsFacebook, BsTwitter, BsInstagram } from 'react-icons/bs'
 
 export default function TextComponent(){
     return (
-        <div>
-            <h1>Build the community that your fans love</h1>
+        <div className='text'>
+            <h1>Build the community your fans will love</h1>
             <p>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussions.</p>
             <button>Register</button>
             <div>
-                <BsFacebook />
-                <BsTwitter />
-                <BsInstagram />
+                <BsFacebook className='fb'/>
+                <BsTwitter className='tr'/>
+                <BsInstagram className='ig'/>
             </div>
         </div>
     )

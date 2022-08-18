3db0072317d412ba2db79cb1e1fc8580f27541a5
oscar anillo <oscarani14@gmail.com>
2022-08-17 19:05:51 -0500

    add animation to image and text component, hover effect

--- src/components/text-component.js
+++ src/components/text-component.js
@@ -1,8 +1,9 @@
-import { BsFacebook, BsTwitter, BsInstagram } from 'react-icons/bs'
+import { BsFacebook, BsTwitter, BsInstagram } from 'react-icons/bs';
+import 'animate.css';
 
 export default function TextComponent(){
     return (
-        <div className='text'>
+        <div className='text animate__animated animate__fadeInDownBig'>
             <h1>Build the community your fans will love</h1>
             <p>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussions.</p>
             <button>Register</button>
